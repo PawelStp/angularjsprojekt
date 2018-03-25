587477d03692d783a9b46ba4a7cca067665bf573
pawel pawel <stypulkowski.pawel1@gmail.com>
2018-03-25 22:48:16 +0200

    3 commit

--- js/components.js
+++ js/components.js
@@ -2,4 +2,4 @@ mainApp
     .component('home', {
         templateUrl: 'templates/home.html',
         controller: 'homeController'
-    });
+    })
--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,7 @@
 angular.module('starter.controllers', [])
-
     .controller('homeController', function ($scope) {
+        this.schoolActivites = [
+            [{ name: "dupa" }, { name: "dupa1" }, { name: "dupa2" }, { name: "dupa3" }, { name: "dupa4" },{}],
+            [{ name: "gowno" }, { name: "gowno1" }, { name: "gowno2" }, { name: "gowno3" }, { name: "dupa4" },{}],
+        ]
     })
